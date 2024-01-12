983cdffdcff0e021aa8ce6856f04a42f15390036
Md Aslam <asmohammad@linkfields.com>
2024-01-13 00:24:05 +0530

    added navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>E-Commerce Dashboard</h1>
-    </div>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<h1>Home Page</h1>} />
+          <Route path="/products" element={<h1>Products Page</h1>} />
+          <Route path="/about" element={<h1>About Page</h1>} />
+          <Route path="/contact" element={<h1>Contact Page</h1>} />
+          <Route path="*" element={<h1>No Page Found</h1>} />
+        </Routes>
+      </div>
   );
 }
 
