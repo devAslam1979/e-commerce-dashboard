7cf503ea3eb7033d5845c4ee2ce628725c8217e6
Md Aslam <asmohammad@linkfields.com>
2024-01-14 21:00:27 +0530

    create login api

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,38 @@
 const express = require("express");
-const cors=require('cors')
+const cors = require("cors");
 require("./db/config");
 const User = require("./db/User");
 
 const app = express();
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 app.post("/signup", async (req, res) => {
   const user = new User(req.body);
-  const result = await user.save();
+  let result = await user.save();
+  result = result.toObject();
+  delete result.password;
   res.send(result);
-  
+});
+app.get("/login", async (req, res) => {
+  if (req.body.email && req.body.password) {
+    const user = await User.findOne({
+      email: req.body.email,
+      password: req.body.password,
+    }).select("-password");
+    if (user) {
+      res.send(user);
+    } else {
+      res.send({
+        error: "User doesn't exist!",
+      });
+    }
+  }else{
+    res.send({
+      error:"Invalid credentials!"
+    })
+  }
 });
 
 app.listen(5000);
